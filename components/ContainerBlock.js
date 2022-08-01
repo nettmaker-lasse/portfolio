8cd63596325a1a2f563ce4568dac8d995258c2a5
nettmaker-lasse <lasse.buus@nettmaker.no>
2022-08-01 12:15:39 +0200

    update fav icon

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -29,7 +29,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
 					rel="canonical"
 					href={`https://yourwebsite.com${router.asPath}`}
 				/>
-				<link rel="shortcut icon" href="/favicon-white.svg" />
+				<link rel="shortcut icon" href="/favicon-me.svg" />
 				<meta property="og:type" content={meta.type} />
 				<meta property="og:site_name" content="Lasse Buus" />
 				<meta property="og:description" content={meta.description} />
