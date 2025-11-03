3b962a2a96421f1cc2e568b7a80ca4d324159023
nettmaker-lasse <lasse.buus@nettmaker.no>
2025-11-03 17:05:15 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import SimpleReactLightbox from "simple-react-lightbox";
 import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider attribute="class" defaultTheme="light">
-			<SimpleReactLightbox>
 				<Component {...pageProps} />
-			</SimpleReactLightbox>
 			<Analytics />
 		</ThemeProvider>
 	);
